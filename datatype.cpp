e4b1359d32d491a8e70154a8d0d9ec71492db95d
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-06-02 15:02:30 +0530

    adding new file

---
+++ datatype.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    // Integer data Type
+
+    int num1 = 18; // it will store smaller range of number
+    long num2 = 233884939393; // it will store larger range of number
+    long long num3 = 84845455454445; // it will store more larger range of number
+
+    cout << num1 <<endl << num2 <<endl << num3 << endl;
+
+    // float and double datatype
+
+    float num4 = 4.5534563;
+    double num5 = 4.34454455444;
+    cout <<num4 << endl << num5 << endl;
+}
