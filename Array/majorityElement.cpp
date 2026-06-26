9b3a52406892fa23d7f45d5e9249e7f31f722fe9
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-06-26 21:17:01 +0530

    majority element

---
+++ Array/majorityElement.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<vector>
+#include<map>
+using namespace std;
+
+// Brute Force approch
+
+// int majorityElement(vector<int> &arr, int n){
+//     for(int i=0; i<n; i++){
+//         int cnt = 0;
+//         for(int j=0; j<n; j++){
+//             if(arr[i] == arr[j]){
+//                 cnt++;
+//             }
+//         }
+//         if(cnt > (n/2)) return arr[i];
+//     }
+//     return -1;
+// }
+
+// Better Approch
+
+int majorityElement(vector<int> &arr, int n){
+    map<int, int> mpp;
+    for(int i=0; i<n; i++){
+        mpp[arr[i]]++;
+    }
+    for(auto it : mpp){
+        if(it.second > (n / 2)){
+            return it.first;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int n;
+    cout<<"Enter size of an array : ";
+    cin>>n;
+    vector<int> arr;
+    cout<<"Enter elements of an array : ";
+    for(int i=0; i<n; i++){
+        int temp;
+        cin>>temp;
+        arr.push_back(temp);
+    }
+    int result = majorityElement(arr, n);
+    cout<<result;
+    return 0;
+}
