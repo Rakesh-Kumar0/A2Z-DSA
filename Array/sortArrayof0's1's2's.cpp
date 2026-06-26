0901a3a2c1ce1df01ea00ab6f6ca12f945801aeb
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-06-26 13:19:07 +0530

    Sort array of 0's 1's and 2's

--- Array/sortArrayof0's1's2's.cpp
+++ Array/sortArrayof0's1's2's.cpp
@@ -10,26 +10,48 @@ using namespace std;
 // }
 
 // Better approch
+// void sortArrayofZeroOneTwo(vector<int> &arr, int n){
+//     int cnt0 = 0;
+//     int cnt1 = 0;
+//     int cnt2 = 0;
+//     for(int i=0; i<n; i++){
+//         if(arr[i] == 0) cnt0 ++;
+//         else if(arr[i] == 1) cnt1++;
+//         else cnt2++;
+//     }
+//     for(int i=0; i<cnt0; i++){
+//         arr[i] = 0;
+//     }
+//     for(int i=cnt0; i<cnt0+cnt1; i++){
+//         arr[i] = 1;
+//     }
+//     for(int i=cnt0+cnt1; i<n; i++){
+//         arr[i] = 2;
+//     }
+// }
+
+
+// Optimal approch using dutch National flag algorithm
+
 void sortArrayofZeroOneTwo(vector<int> &arr, int n){
-    int cnt0 = 0;
-    int cnt1 = 0;
-    int cnt2 = 0;
-    for(int i=0; i<n; i++){
-        if(arr[i] == 0) cnt0 ++;
-        else if(arr[i] == 1) cnt1++;
-        else cnt2++;
-    }
-    for(int i=0; i<cnt0; i++){
-        arr[i] = 0;
-    }
-    for(int i=cnt0; i<cnt0+cnt1; i++){
-        arr[i] = 1;
-    }
-    for(int i=cnt0+cnt1; i<n; i++){
-        arr[i] = 2;
+    int low = 0;
+    int mid = 0;
+    int high = n-1;
+    while(mid <= high){
+        if(arr[mid] == 0){
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        }
+        else if(arr[mid] == 1){
+            mid++;
+        }
+        else if(arr[mid] == 2){
+            swap(arr[mid], arr[high]);
+            high--;
+        }
     }
 }
-
 int main(){
     int n;
     cout<<"Enter size of an array : ";
