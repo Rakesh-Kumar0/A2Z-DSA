7e7b855e44ebb5acfdfb2ca494a654e96a1d850a
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-06-27 23:17:03 +0530

    rearrange elements by sign

--- Array/rearrangeElementBySign.cpp
+++ Array/rearrangeElementBySign.cpp
@@ -3,23 +3,45 @@
 using namespace std;
 
 // Brute force approch
-void rearrangeElementsBySign(vector<int> &arr, int n){
-    vector<int> pos; // it will store positive elements
-    vector<int> neg; // it will store negative elements
+// void rearrangeElementsBySign(vector<int> &arr, int n){
+//     vector<int> pos; // it will store positive elements
+//     vector<int> neg; // it will store negative elements
+//     for(int i=0; i<n; i++){
+//         if(arr[i] > 0){
+//             pos.push_back(arr[i]);
+//         }
+//         else {
+//             neg.push_back(arr[i]);
+
+//         }
+//     }
+//     for(int i=0; i<n/2; i++){
+//         arr[2 * i] = pos[i];
+//         arr[2 * i +1] = neg[i];
+//     }
+// }
+
+
+// Optimal Approch
+
+vector<int> rearrangeElementsBySign(vector<int> &arr, int n){
+    vector<int> ans(n);
+    int posIndex = 0;
+    int negIndex = 1;
     for(int i=0; i<n; i++){
         if(arr[i] > 0){
-            pos.push_back(arr[i]);
+            ans[posIndex] = arr[i];
+            posIndex = posIndex + 2;
         }
         else {
-            neg.push_back(arr[i]);
-
+            ans[negIndex] = arr[i];
+            negIndex = negIndex + 2;
         }
     }
-    for(int i=0; i<n/2; i++){
-        arr[2 * i] = pos[i];
-        arr[2 * i +1] = neg[i];
-    }
+    return ans;
 }
+
+
 
 int main(){
     int n;
@@ -32,8 +54,8 @@ int main(){
         cin>>temp;
         arr.push_back(temp);
     }
-    rearrangeElementsBySign(arr, n);
-    for(auto it : arr){
+    vector<int> result = rearrangeElementsBySign(arr, n);
+    for(auto it : result){
         cout<<it <<" ";
     }
     return 0;
