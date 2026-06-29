b65f77edb079f0836c2d89c8bec0882c055cac63
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-06-29 14:23:17 +0530

    longest consecutive sequence

---
+++ Array/longestConsecutiveSequence.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<climits>
+using namespace std;
+
+// brute force approch
+// linear search function
+bool ls(vector<int> &arr, int num){
+    for(int i=0; i<arr.size(); i++){
+        if(arr[i] == num) return true;
+    }
+    return false;
+}
+
+int longestConsecutiveSequence(vector<int> &arr, int n){
+    int longest = 1;
+    for(int i=0; i<n; i++){
+        int x = arr[i];
+        int cnt = 1;
+        while(ls(arr,x+1) == true){
+            x = x+1;
+            cnt++;
+        }
+        longest = max(longest, cnt);
+    }
+    return longest;
+}
+// Optimal approch
+
+
+int main(){
+    int n;
+    cout<<"Enter size of an array : ";
+    cin>>n;
+    vector<int> arr;
+    cout<<"Enter elements of an array : ";
+    for(int i=0; i<n; i++){
+        int temp;
+        cin>>temp;
+        arr.push_back(temp);
+    }
+
+    int result = longestConsecutiveSequence(arr, n);
+    cout<<result;
+    return 0;
+}
