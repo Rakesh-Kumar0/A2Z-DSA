fa18cc97f46c771c2758789f314d5692af35b776
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-06-29 13:21:46 +0530

    next Permutation

--- Array/nextPermutation.cpp
+++ Array/nextPermutation.cpp
@@ -4,9 +4,38 @@
 using namespace std;
 
 // Better Approch
-void nextPermutation(vector<int> &arr, int n){
-    next_permutation(arr.begin(), arr.end());
+// void nextPermutation(vector<int> &arr, int n){
+//     next_permutation(arr.begin(), arr.end());
+// }
+
+// Implementation of above algorithm
+
+vector<int> nextPermutation(vector<int> &arr, int n){
+    // find break point
+    int index = -1;
+    for(int i=n-2; i>=0; i--){
+        if(arr[i] < arr[i+1]){
+        index = i;
+        break;
+        }
+    }
+    if(index == -1){
+        reverse(arr.begin(), arr.end());
+        return arr;
+    }
+
+    // find number greater than break point
+    for(int i=n-1; i>=index; i--){
+        if(arr[i] > arr[index]){
+            swap(arr[i], arr[index]);
+            break;
+        }
+    }
+    reverse(arr.begin()+index+1, arr.end());
+    return arr;
 }
+
+
 
 int main(){
     int n;
@@ -20,8 +49,8 @@ int main(){
         arr.push_back(temp);
     }
 
-    nextPermutation(arr, n);
-    for(auto it : arr){
+    vector<int> result = nextPermutation(arr, n);
+    for(auto it : result){
         cout<<it <<" ";
     }
     return 0;
