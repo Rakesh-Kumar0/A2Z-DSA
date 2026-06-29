a4dd40100076a694c7990751c8e945804b4a222e
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-06-29 16:30:52 +0530

    longest consecutive sequence

--- Array/longestConsecutiveSequence.cpp
+++ Array/longestConsecutiveSequence.cpp
@@ -6,23 +6,43 @@ using namespace std;
 
 // brute force approch
 // linear search function
-bool ls(vector<int> &arr, int num){
-    for(int i=0; i<arr.size(); i++){
-        if(arr[i] == num) return true;
-    }
-    return false;
-}
+// bool ls(vector<int> &arr, int num){
+//     for(int i=0; i<arr.size(); i++){
+//         if(arr[i] == num) return true;
+//     }
+//     return false;
+// }
 
+// int longestConsecutiveSequence(vector<int> &arr, int n){
+//     int longest = 1;
+//     for(int i=0; i<n; i++){
+//         int x = arr[i];
+//         int cnt = 1;
+//         while(ls(arr,x+1) == true){
+//             x = x+1;
+//             cnt++;
+//         }
+//         longest = max(longest, cnt);
+//     }
+//     return longest;
+// }
+
+// better approch
 int longestConsecutiveSequence(vector<int> &arr, int n){
+    sort(arr.begin(), arr.end());
     int longest = 1;
+    int cnt = 0;
+    int lastSmaller = INT_MIN;
     for(int i=0; i<n; i++){
-        int x = arr[i];
-        int cnt = 1;
-        while(ls(arr,x+1) == true){
-            x = x+1;
+        if(arr[i]-1 == lastSmaller){
             cnt++;
+            lastSmaller = arr[i];
         }
-        longest = max(longest, cnt);
+        else if(arr[i] != lastSmaller){
+            cnt = 1;
+            lastSmaller = arr[i];
+        }
+        longest = max(longest,cnt);
     }
     return longest;
 }
