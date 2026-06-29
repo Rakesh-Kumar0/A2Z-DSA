4e46200524ce1c9877117b1d40fdf50b1901ba5f
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-06-29 16:46:38 +0530

    longest consecutive sequence

--- Array/longestConsecutiveSequence.cpp
+++ Array/longestConsecutiveSequence.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<set>
 #include<climits>
 using namespace std;
 
@@ -28,26 +29,45 @@ using namespace std;
 // }
 
 // better approch
-int longestConsecutiveSequence(vector<int> &arr, int n){
-    sort(arr.begin(), arr.end());
+// int longestConsecutiveSequence(vector<int> &arr, int n){
+//     sort(arr.begin(), arr.end());
+//     int longest = 1;
+//     int cnt = 0;
+//     int lastSmaller = INT_MIN;
+//     for(int i=0; i<n; i++){
+//         if(arr[i]-1 == lastSmaller){
+//             cnt++;
+//             lastSmaller = arr[i];
+//         }
+//         else if(arr[i] != lastSmaller){
+//             cnt = 1;
+//             lastSmaller = arr[i];
+//         }
+//         longest = max(longest,cnt);
+//     }
+//     return longest;
+// }
+
+// Optimal approch
+ int longestConsecutiveSequence(vector<int> &arr, int n){
     int longest = 1;
-    int cnt = 0;
-    int lastSmaller = INT_MIN;
+    set<int> st;
     for(int i=0; i<n; i++){
-        if(arr[i]-1 == lastSmaller){
-            cnt++;
-            lastSmaller = arr[i];
+        st.insert(arr[i]);
+    }
+    for(auto it : st){
+        if(st.find(it-1) == st.end()){
+            int cnt = 1;
+            int x = it;
+            while(st.find(x+1) != st.end()){
+                x = x + 1;
+                cnt = cnt + 1;
+            }
+            longest = max(longest, cnt);
         }
-        else if(arr[i] != lastSmaller){
-            cnt = 1;
-            lastSmaller = arr[i];
-        }
-        longest = max(longest,cnt);
     }
     return longest;
-}
-// Optimal approch
-
+ }
 
 int main(){
     int n;
