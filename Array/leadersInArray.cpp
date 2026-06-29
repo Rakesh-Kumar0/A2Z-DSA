d730f491949cc9ce7e49171179e3006a92546b18
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-06-29 13:59:52 +0530

    leader in an array

--- Array/leadersInArray.cpp
+++ Array/leadersInArray.cpp
@@ -1,21 +1,36 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<climits>
 using namespace std;
 
 // brute force approch
-vector<int> leaderInArray(vector<int> &arr, int n){
+// vector<int> leaderInArray(vector<int> &arr, int n){
+//     vector<int> ans;
+//     for(int i=0; i<n; i++){
+//         int leader = true;
+//         for(int j=i+1; j<n; j++){
+//             if(arr[j] > arr[i]){
+//                 leader = false;
+//                 break;
+//             }
+//         }
+//         if(leader == true)  ans.push_back(arr[i]);
+//     }
+//     return ans;
+// }
+
+// Optimal approch
+vector<int> leaderInArray(vector<int> & arr, int n){
     vector<int> ans;
-    for(int i=0; i<n; i++){
-        int leader = true;
-        for(int j=i+1; j<n; j++){
-            if(arr[j] > arr[i]){
-                leader = false;
-                break;
-            }
+    int maxi = INT_MIN;
+    for(int i=n-1; i>=0; i--){
+        if(arr[i] > maxi){
+            ans.push_back(arr[i]);
         }
-        if(leader == true)  ans.push_back(arr[i]);
+        maxi = max(maxi, arr[i]);
     }
+    sort(ans.begin(), ans.end());
     return ans;
 }
 
