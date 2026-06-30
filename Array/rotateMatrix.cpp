aab3381ce5e137ca1b47210a322af44c2bed43f0
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-06-30 12:18:56 +0530

    rotate matrix by 90 deg

---
+++ Array/rotateMatrix.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<set>
+#include<climits>
+using namespace std;
+
+// brute force approch
+vector<vector<int>> rotateMatrix(vector<vector<int>> &mat, int n, int m){
+    vector<vector<int>> ans(n, vector<int>(m));
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            ans[j][(n-1)-i] = mat[i][j];
+        }
+    }
+    return ans;
+}
+
+
+// better approch
+
+
+
+// Optimal approch
+ 
+
+int main(){
+    int n;
+    cout<<"Enter number of rows : ";
+    cin>>n;
+    int m;
+    cout<<"Enter number of columns : ";
+    cin>>m;
+    vector<vector<int>> mat(n, vector<int>(m));
+    cout<<"Enter elements of an matrix : ";
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin>>mat[i][j];
+        }
+    }
+    vector<vector<int>> result = rotateMatrix(mat,n,m);
+    // Print final matrix
+    for (auto row : result) {
+        for (auto val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
