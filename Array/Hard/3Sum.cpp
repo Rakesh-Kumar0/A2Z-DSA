8baf636bfada28948802884a8105baad0fb3df20
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-07-03 18:12:37 +0530

    3 sum problem

--- Array/Hard/3Sum.cpp
+++ Array/Hard/3Sum.cpp
@@ -26,21 +26,51 @@ using namespace std;
 
 // better approch
 
+// vector<vector<int>> threeSum(vector<int> &arr, int n){
+//     set<vector<int>> st;
+//     for(int i=0; i<n; i++){
+//         set<int> hashset;
+//         for(int j=i+1; j<n; j++){
+//             int third = -(arr[i] + arr[j]);
+//             if(hashset.find(third) != hashset.end()){
+//                 vector<int>temp{arr[i], arr[j], third};
+//                 sort(temp.begin(), temp.end());
+//                 st.insert(temp);
+//             }
+//             hashset.insert(arr[j]);
+//         }
+//     }
+//     vector<vector<int>> ans(st.begin(), st.end());  // it will store all triplets in sorted order
+//     return ans;
+// }
+
+// optimal approch
+
 vector<vector<int>> threeSum(vector<int> &arr, int n){
-    set<vector<int>> st;
+    vector<vector<int>> ans;
+    sort(arr.begin(), arr.end());
     for(int i=0; i<n; i++){
-        set<int> hashset;
-        for(int j=i+1; j<n; j++){
-            int third = -(arr[i] + arr[j]);
-            if(hashset.find(third) != hashset.end()){
-                vector<int>temp{arr[i], arr[j], third};
-                sort(temp.begin(), temp.end());
-                st.insert(temp);
+        if(i>0 && arr[i] == arr[i-1]) continue;
+        int j = i+1;
+        int k = n-1;
+        while(j < k){
+            int sum = arr[i]+arr[j]+arr[k];
+            if(sum > 0){
+                k--;
             }
-            hashset.insert(arr[j]);
+            else if(sum < 0){
+                j++;
+            }
+            else {
+                vector<int> temp = {arr[i] , arr[j], arr[k]};
+                ans.push_back(temp);
+                j++;
+                k--;
+                while(j <k && arr[j] == arr[j-1]) j++;
+                while(j<k && arr[k] == arr[k+1]) k--;
+            }
         }
     }
-    vector<vector<int>> ans(st.begin(), st.end());  // it will store all triplets in sorted order
     return ans;
 }
 
