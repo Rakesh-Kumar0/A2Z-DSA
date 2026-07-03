d3b8c24242bd6c54f7e75c1eb757fc811dc8a684
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-07-03 23:18:57 +0530

    4 sum problem

---
+++ Array/Hard/4Sum.cpp
@@ -0,0 +1,98 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<set>
+#include<climits>
+#include<algorithm>
+using namespace std;
+
+// brute force approch
+vector<vector<int>> fourSum(vector<int> &arr, int n){
+    set<vector<int>> st;
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            for(int k=j+1; k<n; k++){
+                for(int l=k+1; l<n; l++){
+                    if(arr[i]+arr[j]+ arr[k]+arr[l] == 0){
+                    vector<int> temp{arr[i], arr[j], arr[k], arr[l]};
+                    sort(temp.begin(), temp.end());
+                    st.insert(temp);
+                }
+                }
+            }
+        }
+    }
+    vector<vector<int>> ans(st.begin(), st.end());  // it will store all triplets in sorted order
+    return ans;
+}
+
+// better approch
+
+// vector<vector<int>> threeSum(vector<int> &arr, int n){
+//     set<vector<int>> st;
+//     for(int i=0; i<n; i++){
+//         set<int> hashset;
+//         for(int j=i+1; j<n; j++){
+//             int third = -(arr[i] + arr[j]);
+//             if(hashset.find(third) != hashset.end()){
+//                 vector<int>temp{arr[i], arr[j], third};
+//                 sort(temp.begin(), temp.end());
+//                 st.insert(temp);
+//             }
+//             hashset.insert(arr[j]);
+//         }
+//     }
+//     vector<vector<int>> ans(st.begin(), st.end());  // it will store all triplets in sorted order
+//     return ans;
+// }
+
+// optimal approch
+
+// vector<vector<int>> threeSum(vector<int> &arr, int n){
+//     vector<vector<int>> ans;
+//     sort(arr.begin(), arr.end());
+//     for(int i=0; i<n; i++){
+//         if(i>0 && arr[i] == arr[i-1]) continue;
+//         int j = i+1;
+//         int k = n-1;
+//         while(j < k){
+//             int sum = arr[i]+arr[j]+arr[k];
+//             if(sum > 0){
+//                 k--;
+//             }
+//             else if(sum < 0){
+//                 j++;
+//             }
+//             else {
+//                 vector<int> temp = {arr[i] , arr[j], arr[k]};
+//                 ans.push_back(temp);
+//                 j++;
+//                 k--;
+//                 while(j <k && arr[j] == arr[j-1]) j++;
+//                 while(j<k && arr[k] == arr[k+1]) k--;
+//             }
+//         }
+//     }
+//     return ans;
+// }
+
+int main(){
+    int n;
+    cout<<"Enter size of an array : ";
+    cin>>n;
+    vector<int> arr;
+    cout<<"Enter elements of an array : ";
+    for(int i=0; i<n; i++){
+        int temp;
+        cin>>temp;
+        arr.push_back(temp);
+    }
+    vector<vector<int>> result = fourSum(arr, n);
+    for(auto row : result){
+       for(auto value : row){
+         cout<<value <<" ";
+       }
+       cout<<endl;
+    }
+    return 0;
+}
