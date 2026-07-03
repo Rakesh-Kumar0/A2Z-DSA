314a0961a024370596602b45466f2d485fd964e7
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-07-03 23:38:06 +0530

    4 sum problem

--- Array/Hard/4Sum.cpp
+++ Array/Hard/4Sum.cpp
@@ -27,56 +27,60 @@ using namespace std;
 // }
 
 // better approch
-vector<vector<int>> fourSum(vector<int> &arr, int n, int target){
-    set<vector<int>> st;
-    for(int i=0; i<n; i++){
-        for(int j=i+1; j<n; j++){
-            set<long long> hashset;
-            for(int k=j+1; k<n; k++){
-                int sum = arr[i] + arr[j] + arr[k];
-                int fourth = target -sum;
-                if(hashset.find(fourth) != hashset.end()){
-                    vector<int>temp = {arr[i], arr[j], arr[k], fourth};
-                    sort(temp.begin(), temp.end());
-                    st.insert(temp);
-            }
-            hashset.insert(arr[k]);
-            }
-        }
-    }
-    vector<vector<int>> ans(st.begin(), st.end());  // it will store all triplets in sorted order
-    return ans;
-}
-
-// optimal approch
-
-// vector<vector<int>> threeSum(vector<int> &arr, int n){
-//     vector<vector<int>> ans;
-//     sort(arr.begin(), arr.end());
+// vector<vector<int>> fourSum(vector<int> &arr, int n, int target){
+//     set<vector<int>> st;
 //     for(int i=0; i<n; i++){
-//         if(i>0 && arr[i] == arr[i-1]) continue;
-//         int j = i+1;
-//         int k = n-1;
-//         while(j < k){
-//             int sum = arr[i]+arr[j]+arr[k];
-//             if(sum > 0){
-//                 k--;
+//         for(int j=i+1; j<n; j++){
+//             set<long long> hashset;
+//             for(int k=j+1; k<n; k++){
+//                 int sum = arr[i] + arr[j] + arr[k];
+//                 int fourth = target -sum;
+//                 if(hashset.find(fourth) != hashset.end()){
+//                     vector<int>temp = {arr[i], arr[j], arr[k], fourth};
+//                     sort(temp.begin(), temp.end());
+//                     st.insert(temp);
 //             }
-//             else if(sum < 0){
-//                 j++;
-//             }
-//             else {
-//                 vector<int> temp = {arr[i] , arr[j], arr[k]};
-//                 ans.push_back(temp);
-//                 j++;
-//                 k--;
-//                 while(j <k && arr[j] == arr[j-1]) j++;
-//                 while(j<k && arr[k] == arr[k+1]) k--;
+//             hashset.insert(arr[k]);
 //             }
 //         }
 //     }
+//     vector<vector<int>> ans(st.begin(), st.end());  // it will store all triplets in sorted order
 //     return ans;
 // }
+
+
+// optimal approch
+
+vector<vector<int>> fourSum(vector<int> &arr, int n, int target){
+    vector<vector<int>> ans;
+    sort(arr.begin(), arr.end());
+    for(int i=0; i<n; i++){
+        if(i>0 && arr[i] == arr[i-1]) continue;
+        for(int j=i+1; j<n; j++){
+            if(j != i+1 && arr[j] == arr[j-1]) continue;
+            int k = j+1;
+            int l = n-1;
+            while(k < l){
+                int sum = arr[i]+arr[j]+arr[k]+arr[l];
+                if(sum == target){
+                    vector<int> temp = {arr[i] , arr[j], arr[k], arr[l]};
+                    ans.push_back(temp);
+                    k++;
+                    l--;
+                    while(k < l && arr[k] == arr[k-1]) k++;
+                    while(k < l && arr[l] == arr[l+1]) l--;
+                }
+                else if(sum < target){
+                    k++;
+                }
+                else {
+                    l--;
+                }
+            }
+        }
+    }
+    return ans;
+}
 
 int main(){
     int n;
