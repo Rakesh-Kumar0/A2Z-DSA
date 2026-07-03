2c61f1ad58276d142623d0dbdf934be3473ab786
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-07-03 17:36:06 +0530

    3 sum problem

--- Array/Hard/3Sum.cpp
+++ Array/Hard/3Sum.cpp
@@ -7,42 +7,42 @@
 using namespace std;
 
 // brute force approch
-vector<vector<int>> threeSum(vector<int> &arr, int n){
-    set<vector<int>> st;
-    for(int i=0; i<n; i++){
-        for(int j=i+1; j<n; j++){
-            for(int k=j+1; k<n; k++){
-                if(arr[i]+arr[j]+ arr[k] == 0){
-                    vector<int> temp{arr[i], arr[j], arr[k]};
-                    sort(temp.begin(), temp.end());
-                    st.insert(temp);
-                }
-            }
-        }
-    }
-    vector<vector<int>> ans(st.begin(), st.end());  // it will store all triplets in sorted order
-    return ans;
-}
-
-// better approch
-
 // vector<vector<int>> threeSum(vector<int> &arr, int n){
 //     set<vector<int>> st;
 //     for(int i=0; i<n; i++){
-//         set<int> hashset;
 //         for(int j=i+1; j<n; j++){
-//             int third = -(arr[i] + arr[j]);
-//             if(hashset.find(third) != hashset.end()){
-//                 vector<int>temp{arr[i], arr[j], third};
-//                 sort(temp.begin(), temp.end());
-//                 st.insert(temp);
+//             for(int k=j+1; k<n; k++){
+//                 if(arr[i]+arr[j]+ arr[k] == 0){
+//                     vector<int> temp{arr[i], arr[j], arr[k]};
+//                     sort(temp.begin(), temp.end());
+//                     st.insert(temp);
+//                 }
 //             }
-//             hashset.insert(arr[j]);
 //         }
 //     }
 //     vector<vector<int>> ans(st.begin(), st.end());  // it will store all triplets in sorted order
 //     return ans;
 // }
+
+// better approch
+
+vector<vector<int>> threeSum(vector<int> &arr, int n){
+    set<vector<int>> st;
+    for(int i=0; i<n; i++){
+        set<int> hashset;
+        for(int j=i+1; j<n; j++){
+            int third = -(arr[i] + arr[j]);
+            if(hashset.find(third) != hashset.end()){
+                vector<int>temp{arr[i], arr[j], third};
+                sort(temp.begin(), temp.end());
+                st.insert(temp);
+            }
+            hashset.insert(arr[j]);
+        }
+    }
+    vector<vector<int>> ans(st.begin(), st.end());  // it will store all triplets in sorted order
+    return ans;
+}
 
 int main(){
     int n;
