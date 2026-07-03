5991d2dc9fb952fba681760ee2852e75c0a89320
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-07-03 16:10:26 +0530

    majority elements

--- Array/Hard/majorityElement2.cpp
+++ Array/Hard/majorityElement2.cpp
@@ -1,21 +1,38 @@
 #include<iostream>
 #include<vector>
+#include<map>
 using namespace std;
 
 // Brute force approch
+// vector<int> majorityElement(vector<int> &arr, int n){
+//     vector<int> list;
+//     for(int i=0; i<n; i++){
+//         if(list.size() == 0 || list[0] != arr[i]){
+//             int cnt = 0;
+//             for(int j=0; j<n; j++){
+//                 if(arr[i] == arr[j]){
+//                     cnt++;
+//                 }
+//             }
+//             if(cnt > n/3){
+//                 list.push_back(arr[i]);
+//             }
+//         }
+//         if(list.size() == 2) break;
+//     }
+//     return list;
+// }
+
+// better approch
+
 vector<int> majorityElement(vector<int> &arr, int n){
     vector<int> list;
+    map<int, int> mpp;
+    int mini = int(n/3) +1;
     for(int i=0; i<n; i++){
-        if(list.size() == 0 || list[0] != arr[i]){
-            int cnt = 0;
-            for(int j=0; j<n; j++){
-                if(arr[i] == arr[j]){
-                    cnt++;
-                }
-            }
-            if(cnt > n/3){
-                list.push_back(arr[i]);
-            }
+        mpp[arr[i]]++;
+        if(mpp[arr[i]]==mini){
+            list.push_back(arr[i]);
         }
         if(list.size() == 2) break;
     }
