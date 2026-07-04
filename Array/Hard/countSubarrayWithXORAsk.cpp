624d1c83555bb9c568253f6dd3c9ab09bbb10d7a
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-07-04 15:39:12 +0530

    count subarray with XOR as k

---
+++ Array/Hard/countSubarrayWithXORAsk.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<map>
+using namespace std;
+
+int countSubArrayWithXORasK(vector<int> &arr, int n, int k){
+   int cnt = 0;
+   for(int i=0; i<n; i++){
+        for(int j=i; j<n; j++){
+            int XOR = 0;
+            for(int k=i; k<=j; k++){
+                XOR = XOR ^ arr[k];
+            }
+            if(XOR == k) cnt++;
+        }
+   }
+   return cnt;
+
+}
+int main(){
+    int n;
+    cout<<"Enter size of an array : ";
+    cin>>n;
+    vector<int> arr;
+    cout<<"Enter elements of an array : ";
+    for(int i=0; i<n; i++){
+        int temp;
+        cin>>temp;
+        arr.push_back(temp);
+    }
+    int k;
+    cout<<"Enter value of k : ";
+    cin>>k;
+    int result = countSubArrayWithXORasK(arr, n, k);
+    cout<<result;
+    return 0;
+}
