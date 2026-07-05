f72565212cc231e9805e40f15f40a3c7a56c809d
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-07-05 12:29:53 +0530

    merge two sorted array

--- Array/Hard/mergeTwoSortedArray.cpp
+++ Array/Hard/mergeTwoSortedArray.cpp
@@ -5,36 +5,55 @@
 using namespace std;
 
 // brute force approch
+// void merge(int arr1[], int arr2[], int n, int m){
+//     int arr3[n+m];
+//     int left=0;
+//     int right=0; 
+//     int index=0;
+//     while(left<n && right <m){
+//         if(arr1[left] <= arr2[right]){
+//             arr3[index] = arr1[left];
+//             left++;
+//             index++;
+//         }
+//         else{
+//             arr3[index] = arr2[right];
+//             index++;
+//             right++;
+//         }
+//     }
+//     // for remaining elements in either array
+//     while(left < n){
+//         arr3[index++] = arr1[left++];
+//     }
+//     while(right < m){
+//         arr3[index++] = arr2[right++];
+//     }
+
+//     // put back again
+//     for(int i=0; i<n+m; i++){
+//         if(i<n) arr1[i] = arr3[i];
+//         else arr2[i-n] = arr3[i];
+//     }
+// }
+
+// better approch
+
 void merge(int arr1[], int arr2[], int n, int m){
-    int arr3[n+m];
-    int left=0;
+    int left=n-1;
     int right=0; 
-    int index=0;
-    while(left<n && right <m){
-        if(arr1[left] <= arr2[right]){
-            arr3[index] = arr1[left];
-            left++;
-            index++;
-        }
-        else{
-            arr3[index] = arr2[right];
-            index++;
+    while(left>0 && right <m){
+        if(arr1[left] >= arr2[right]){
+            swap(arr1[left], arr2[right]);
+            left--;
             right++;
         }
+        else{
+           break;
+        }
     }
-    // for remaining elements in either array
-    while(left < n){
-        arr3[index++] = arr1[left++];
-    }
-    while(right < m){
-        arr3[index++] = arr2[right++];
-    }
-
-    // put back again
-    for(int i=0; i<n+m; i++){
-        if(i<n) arr1[i] = arr3[i];
-        else arr2[i-n] = arr3[i];
-    }
+    sort(arr1, arr1+n);
+    sort(arr2, arr2+m);
 }
 
 int main(){
