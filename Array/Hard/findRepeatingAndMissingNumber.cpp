7719bee57ecc9e812c7186a8e63f51a79a099474
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-07-06 10:21:50 +0530

    finding repeating and missing number

--- Array/Hard/findRepeatingAndMissingNumber.cpp
+++ Array/Hard/findRepeatingAndMissingNumber.cpp
@@ -1,23 +1,37 @@
 #include<iostream>
 #include<vector>
-#include<map>
-#include<algorithm>
-#include<set>
 using namespace std;
 
 //brute force approch
+// vector<int> repeatingAndMissingNumber(vector<int> &arr, int n){
+//     int repeating = -1;
+//     int missing = -1;
+//     for(int i=1; i<=n; i++){
+//         int cnt = 0;
+//         for(int j=0; j<n; j++){
+//             if(arr[j] == i){
+//                 cnt++;
+//             }
+//         }
+//         if(cnt == 2) repeating = i;
+//         else if(cnt == 0) missing = i;
+//         if(repeating != -1 && missing != -1) break;
+//     }
+//     return {repeating,missing};
+// }
+
+// better approch
+
 vector<int> repeatingAndMissingNumber(vector<int> &arr, int n){
+    int hash[n+1] = {0};
     int repeating = -1;
     int missing = -1;
+    for(int i=0; i<n; i++){
+        hash[arr[i]]++;
+    }
     for(int i=1; i<=n; i++){
-        int cnt = 0;
-        for(int j=0; j<n; j++){
-            if(arr[j] == i){
-                cnt++;
-            }
-        }
-        if(cnt == 2) repeating = i;
-        else if(cnt == 0) missing = i;
+        if(hash[i] == 2) repeating = i;
+        else if(hash[i] == 0) missing = i;
         if(repeating != -1 && missing != -1) break;
     }
     return {repeating,missing};
