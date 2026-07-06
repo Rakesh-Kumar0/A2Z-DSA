aabd093b9273440e7c71a62ac35f44777dbdf62b
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-07-06 11:44:38 +0530

    count inversion

--- Array/Hard/countInversion.cpp
+++ Array/Hard/countInversion.cpp
@@ -3,61 +3,61 @@
 using namespace std;
 
 //brute force approch
-// int countInversion(vector<int> &arr, int n){
-//     int cnt = 0;
-//     for(int i=0; i<n; i++){
-//         for(int j=i+1; j<n; j++){
-//             if(arr[i] > arr[j]){
-//                 cnt++;
-//             }
-//         }
-//     }
-//     return cnt;
-// }
+int countInversion(vector<int> &arr, int n){
+    int cnt = 0;
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            if(arr[i] > arr[j]){
+                cnt++;
+            }
+        }
+    }
+    return cnt;
+}
 
 // optimal approch
 
-int cnt = 0;
-void merge(vector<int>& arr, int low, int mid, int high) {
-        vector<int> temp;
-        int left = low, right = mid + 1;
-        while (left <= mid && right <= high) {
-            if (arr[left] <= arr[right])
-                temp.push_back(arr[left++]);
-            // right is smaller
-            else{
-                temp.push_back(arr[right]);
-                cnt += (mid -left +1);
-                right++;
-            }
-        }
-        while (left <= mid)
-            temp.push_back(arr[left++]);
+// int cnt = 0;
+// void merge(vector<int>& arr, int low, int mid, int high) {
+//         vector<int> temp;
+//         int left = low, right = mid + 1;
+//         while (left <= mid && right <= high) {
+//             if (arr[left] <= arr[right])
+//                 temp.push_back(arr[left++]);
+//             // right is smaller
+//             else{
+//                 temp.push_back(arr[right]);
+//                 cnt += (mid -left +1);
+//                 right++;
+//             }
+//         }
+//         while (left <= mid)
+//             temp.push_back(arr[left++]);
 
-        while (right <= high)
-            temp.push_back(arr[right++]);
+//         while (right <= high)
+//             temp.push_back(arr[right++]);
 
-        for (int i = low; i <= high; i++)
-            arr[i] = temp[i - low];
-    }
+//         for (int i = low; i <= high; i++)
+//             arr[i] = temp[i - low];
+//     }
 
-    void mergeSort(vector<int>& arr, int low, int high) {
-        if (low >= high)
-            return;
+//     void mergeSort(vector<int>& arr, int low, int high) {
+//         if (low >= high)
+//             return;
 
-        int mid = (low + high) / 2;
+//         int mid = (low + high) / 2;
 
-        mergeSort(arr, low, mid);
+//         mergeSort(arr, low, mid);
 
-        mergeSort(arr, mid + 1, high);
+//         mergeSort(arr, mid + 1, high);
 
-        merge(arr, low, mid, high);
-    }
+//         merge(arr, low, mid, high);
+//     }
 
-int countInversion(vector<int> &arr, int n){
-    mergeSort(arr, 0, n-1);
-    return cnt;
-}
+// int countInversion(vector<int> &arr, int n){
+//     mergeSort(arr, 0, n-1);
+//     return cnt;
+// }
 int main(){
     int n;
     cout<<"Enter size of an array : ";
