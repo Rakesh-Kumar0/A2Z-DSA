8e7815ebab76d0cb97e3336f553fc847a48a8836
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-07-06 10:51:56 +0530

    finding repeating and missing number

--- Array/Hard/findRepeatingAndMissingNumber.cpp
+++ Array/Hard/findRepeatingAndMissingNumber.cpp
@@ -22,19 +22,36 @@ using namespace std;
 
 // better approch
 
+// vector<int> repeatingAndMissingNumber(vector<int> &arr, int n){
+//     int hash[n+1] = {0};
+//     int repeating = -1;
+//     int missing = -1;
+//     for(int i=0; i<n; i++){
+//         hash[arr[i]]++;
+//     }
+//     for(int i=1; i<=n; i++){
+//         if(hash[i] == 2) repeating = i;
+//         else if(hash[i] == 0) missing = i;
+//         if(repeating != -1 && missing != -1) break;
+//     }
+//     return {repeating,missing};
+// }
+
+// optimal approch 1
 vector<int> repeatingAndMissingNumber(vector<int> &arr, int n){
-    int hash[n+1] = {0};
-    int repeating = -1;
-    int missing = -1;
+    long long sn = (n*(n+1))/2;
+    long long s2n = (n*(n+1)*(2*n+1))/6;
+    long long s = 0, s2 = 0;
     for(int i=0; i<n; i++){
-        hash[arr[i]]++;
+        s += arr[i];
+        s2 += (long long)arr[i]*(long long)arr[i];
     }
-    for(int i=1; i<=n; i++){
-        if(hash[i] == 2) repeating = i;
-        else if(hash[i] == 0) missing = i;
-        if(repeating != -1 && missing != -1) break;
-    }
-    return {repeating,missing};
+    long long val1 = s - sn;
+    long long val2 = s2 - s2n;
+    val2 = val2/val1;
+    long long x = (val1 + val2)/2;
+    long long y = x - val1;
+    return {(int)x , (int)y};
 }
 
 int main(){
