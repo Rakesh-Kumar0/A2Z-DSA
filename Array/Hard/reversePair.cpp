570164732975d35842a4812b7e35137af3787223
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-07-07 11:11:25 +0530

    reverse pair

---
+++ Array/Hard/reversePair.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//brute force approch
+int reversePair(vector<int> &arr, int n){
+    int cnt = 0;
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            if(arr[i] > 2 * arr[j]){
+                cnt++;
+            }
+        }
+    }
+    return cnt;
+}
+
+
+int main(){
+    int n;
+    cout<<"Enter size of an array : ";
+    cin>>n;
+    vector<int> arr;
+    cout<<"Enter elements of an ayyar : ";
+    for(int i=0; i<n; i++){
+        int temp;
+        cin>>temp;
+        arr.push_back(temp);
+    }
+    int result = reversePair(arr, n);
+    cout<<result;
+
+    return 0;
+}
