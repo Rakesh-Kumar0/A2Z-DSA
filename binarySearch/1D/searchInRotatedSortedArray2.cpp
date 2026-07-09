fbc9d8e6d8956b15686786b7ede282630bca4793
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-07-09 23:27:47 +0530

    search in rotated sorted array if duplicate are present

--- binarySearch/1D/searchInRotatedSortedArray2.cpp
+++ binarySearch/1D/searchInRotatedSortedArray2.cpp
@@ -3,39 +3,44 @@
 using namespace std;
 
 //brute force approch
-bool searchInRotatedSortedArray(vector<int> &arr, int n, int target){
-    for(int i=0; i<n; i++){
-        if(arr[i] == target){
-            return true;
-        }
-    }
-    return false;
-}
-
-// binary search
-// int searchInRotatedSortedArray(vector<int> &arr, int n, int target){
-//     int low = 0;
-//     int high = n-1;
-//     while(low <= high){
-//         int mid = (low + high) / 2;
-//         if(arr[mid] == target) return mid;
-//         // left sorted
-//         if(arr[low] <= arr[mid]){
-//             if(arr[low] <= target && target <= arr[mid]){
-//                 high = mid -1;
-//             }
-//             else low = mid + 1;
-//         }
-//         // right sorted
-//         else {
-//             if(arr[mid] <= target && target <= arr[high]){
-//                 low = mid + 1;
-//             }
-//             else high = mid -1;
+// bool searchInRotatedSortedArray(vector<int> &arr, int n, int target){
+//     for(int i=0; i<n; i++){
+//         if(arr[i] == target){
+//             return true;
 //         }
 //     }
-//     return -1;
+//     return false;
 // }
+
+// binary search
+bool searchInRotatedSortedArray(vector<int> &arr, int n, int target){
+    int low = 0;
+    int high = n-1;
+    while(low <= high){
+        int mid = (low + high) / 2;
+        if(arr[mid] == target) return true;
+        if(arr[low] == arr[mid] && arr[mid] == arr[high]){
+            low++;
+            high--;
+            continue;// trim down thar condition
+        }
+        // left sorted
+        if(arr[low] <= arr[mid]){
+            if(arr[low] <= target && target <= arr[mid]){
+                high = mid -1;
+            }
+            else low = mid + 1;
+        }
+        // right sorted
+        else {
+            if(arr[mid] <= target && target <= arr[high]){
+                low = mid + 1;
+            }
+            else high = mid -1;
+        }
+    }
+    return -1;
+}
 
 int main(){
     int n;
