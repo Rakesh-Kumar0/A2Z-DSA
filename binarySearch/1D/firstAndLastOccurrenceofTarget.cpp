332475ad0128184968d44448a62e311b881a2a78
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-07-09 19:10:15 +0530

    first and last occurrence of target

--- binarySearch/1D/firstAndLastOccurrenceofTarget.cpp
+++ binarySearch/1D/firstAndLastOccurrenceofTarget.cpp
@@ -17,41 +17,81 @@ using namespace std;
 
 // binary Search
 
-int lowerBound(vector<int> &arr, int n, int target){
+// int lowerBound(vector<int> &arr, int n, int target){
+//     int low = 0;
+//     int high = n-1;
+//     int ans = n;
+//     while(low <= high){
+//         int mid = (low+high)/2;
+//         if(arr[mid] >= target){
+//             ans = mid;
+//             high = mid - 1;
+//         }
+//         else low = mid + 1;
+//     }
+//     return ans;
+// }
+
+// int upperBound(vector<int> &arr, int n, int target){
+//     int low = 0;
+//     int high = n-1;
+//     int ans = n;
+//     while(low <= high){
+//         int mid = (low+high)/2;
+//         if(arr[mid] > target){
+//             ans = mid;
+//             high = mid - 1;
+//         }
+//         else low = mid + 1;
+//     }
+//     return ans;
+// }
+// pair<int, int> firstAndLastPosition(vector<int> &arr, int n, int target){
+//     int lb = lowerBound(arr,n,target);
+//     if(lb == n || arr[lb] != target) return {-1, -1};
+//     return {lb, upperBound(arr,n,target)-1};
+// }
+
+
+// without use of lowerBound and upperBoud
+int firstPosition(vector<int> &arr, int n, int target){
     int low = 0;
     int high = n-1;
-    int ans = n;
-    while(low <= high){
+    int first = -1;
+    while(low<=high){
         int mid = (low+high)/2;
-        if(arr[mid] >= target){
-            ans = mid;
+        if(arr[mid] == target){
+            first = mid;
             high = mid - 1;
         }
-        else low = mid + 1;
+        else if(arr[mid] < target) low = mid + 1;
+        else high = mid -1 ;
     }
-    return ans;
+    return first;
 }
 
-int upperBound(vector<int> &arr, int n, int target){
+int lastPosition(vector<int> &arr, int n, int target){
     int low = 0;
     int high = n-1;
-    int ans = n;
-    while(low <= high){
+    int last = -1;
+    while(low<=high){
         int mid = (low+high)/2;
-        if(arr[mid] > target){
-            ans = mid;
-            high = mid - 1;
+        if(arr[mid] == target){
+            last = mid;
+            low  = mid + 1;
         }
-        else low = mid + 1;
+        else if(arr[mid] < target) low = mid + 1;
+        else high = mid -1 ;
     }
-    return ans;
+    return last;
 }
+
 pair<int, int> firstAndLastPosition(vector<int> &arr, int n, int target){
-    int lb = lowerBound(arr,n,target);
-    if(lb == n || arr[lb] != target) return {-1, -1};
-    return {lb, upperBound(arr,n,target)-1};
+    int first = firstPosition(arr, n, target);
+    if(first == -1) return {-1, -1};
+    int last = lastPosition(arr,n,target);
+    return {first, last};
 }
-
 int main(){
     int n;
     cout<<"Enter size of an array : ";
