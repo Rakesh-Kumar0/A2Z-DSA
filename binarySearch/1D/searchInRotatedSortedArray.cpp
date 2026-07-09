8458c30bcfcacdb1b15b14e9cfb78c1577fd6ca3
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-07-09 22:56:05 +0530

    search in rotated sorted array

--- binarySearch/1D/searchInRotatedSortedArray.cpp
+++ binarySearch/1D/searchInRotatedSortedArray.cpp
@@ -3,10 +3,35 @@
 using namespace std;
 
 // brute force approch
+// int searchInRotatedSortedArray(vector<int> &arr, int n, int target){
+//     for(int i=0; i<n; i++){
+//         if(arr[i] == target){
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
+
+// binary search
 int searchInRotatedSortedArray(vector<int> &arr, int n, int target){
-    for(int i=0; i<n; i++){
-        if(arr[i] == target){
-            return i;
+    int low = 0;
+    int high = n-1;
+    while(low <= high){
+        int mid = (low + high) / 2;
+        if(arr[mid] == target) return mid;
+        // left sorted
+        if(arr[low] <= arr[mid]){
+            if(arr[low] <= target && target <= arr[mid]){
+                high = mid -1;
+            }
+            else low = mid + 1;
+        }
+        // right sorted
+        else {
+            if(arr[mid] <= target && target <= arr[high]){
+                low = mid + 1;
+            }
+            else high = mid -1;
         }
     }
     return -1;
