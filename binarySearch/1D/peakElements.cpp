fdcb88d116ed471c47e6fceec57acef10b4bff94
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-07-10 13:08:43 +0530

    peak element

--- binarySearch/1D/peakElements.cpp
+++ binarySearch/1D/peakElements.cpp
@@ -4,14 +4,28 @@
 using namespace std;
 
 // brute force approch
+// int peakElement(vector<int> &arr, int n){
+//     for(int i=0; i<n; i++){
+//         if((i==0 || arr[i-1]<arr[i]) && (i == n-1 || arr[i] > arr[i+1])) return arr[i];
+//     }
+//     return -1;
+// }
+
+// binary search
 int peakElement(vector<int> &arr, int n){
-    for(int i=0; i<n; i++){
-        if((i==0 || arr[i-1]<arr[i]) && (i == n-1 || arr[i] > arr[i+1])) return arr[i];
+    if(n == 1) return arr[0];
+    if(arr[0] > arr[1]) return arr[0];
+    if(arr[n-1]>arr[n-2]) return arr[n-1];
+    int low = 1;
+    int high = n-2;
+    while(low <= high){
+        int mid = (low+ high)/2;
+        if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]) return arr[mid];
+        else if(arr[mid]>arr[mid-1]) low = mid+1;
+        else if(arr[mid]>arr[mid+1]) high = mid-1;
     }
     return -1;
 }
-
-// binary search
 
 
 int main(){
