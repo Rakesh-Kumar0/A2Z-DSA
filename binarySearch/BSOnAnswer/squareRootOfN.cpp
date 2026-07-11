37676cf0fb8db3e169b764451d9316174ffd5815
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-07-11 13:06:49 +0530

    square root

--- binarySearch/BSOnAnswer/squareRootOfN.cpp
+++ binarySearch/BSOnAnswer/squareRootOfN.cpp
@@ -3,14 +3,31 @@
 using namespace std;
 
 // brute force 
+// int squareRoot(int n){
+//     int ans = 1;
+//     for(int i=1; i<=n; i++){
+//         if(i * i <=n) ans = i;
+//         else break;
+//     }
+//     return ans;
+// }
+
+// binary search
 int squareRoot(int n){
     int ans = 1;
-    for(int i=1; i<=n; i++){
-        if(i * i <=n) ans = i;
-        else break;
+    int low = 1;
+    int high = n;
+    while(low <= high){
+        int mid = (low + high)/2;
+        if(mid * mid <= n) {
+            ans = mid;
+            low = mid + 1;
+        }
+        else high = mid - 1;
     }
     return ans;
 }
+   
 
 int main(){
     int n;
