a40b350460854683aa4b97182232a22783d8a2a2
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-07-20 17:55:15 +0530

    smallest divisior

--- binarySearch/BSOnAnswer/smallestDivisorGivenaThreshold.cpp
+++ binarySearch/BSOnAnswer/smallestDivisorGivenaThreshold.cpp
@@ -7,16 +7,35 @@
 using namespace std;
 
 // brute force approch
-int smallestDivisor(vector<int> &arr, int threshold){
-    int maxi = *max_element(arr.begin(), arr.end());
-    for(int div=1; div<=maxi; div++){
-        int sum = 0;
-        for(int i=0; i<arr.size(); i++){
-            sum = sum + ceil((double)arr[i]/(double)div);
-        }
-        if(sum <= threshold) return div;
+// int smallestDivisor(vector<int> &arr, int threshold){
+//     int maxi = *max_element(arr.begin(), arr.end());
+//     for(int div=1; div<=maxi; div++){
+//         int sum = 0;
+//         for(int i=0; i<arr.size(); i++){
+//             sum = sum + ceil((double)arr[i]/(double)div);
+//         }
+//         if(sum <= threshold) return div;
+//     }
+//     return -1;
+// }
+
+//binary search
+int sumOfDivisor(vector<int> &arr, int div){
+    int sum = 0;
+    for(int i=0; i<arr.size(); i++){
+        sum +=ceil((double)arr[i]/(double)div);
     }
-    return -1;
+    return sum;
+}
+int smallestDivisor(vector<int> &arr, int threshold){
+    int low = 1;
+    int high = *max_element(arr.begin(), arr.end());
+    while(low <= high){
+        int mid = low + (high - low)/2;
+        if(sumOfDivisor(arr, mid) <= threshold) high = mid -1;
+        else low = mid+1;
+    }
+    return low;
 }
 
 
