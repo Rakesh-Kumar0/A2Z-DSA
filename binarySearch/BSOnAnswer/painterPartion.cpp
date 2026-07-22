c719e9afde2b2bb9ab152b6c1bb81fecd4d5458d
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-07-22 14:23:32 +0530

    painter's partition

--- binarySearch/BSOnAnswer/painterPartion.cpp
+++ binarySearch/BSOnAnswer/painterPartion.cpp
@@ -25,34 +25,34 @@ int countPainter(vector<int> &arr, int time){
 
 // brute force approch
 // Function to find the minimum time to paint all boards using at most k painters
-int findtime(vector<int> &arr, int n, int painter){
-    // If students are more than books, allocation is impossible.
-    if(painter > n) return -1;
-    // Minimum possible answer is the largest book.
-    int low = *max_element(arr.begin(), arr.end());
+// int findtime(vector<int> &arr, int n, int painter){
+//     // If students are more than books, allocation is impossible.
+//     if(painter > n) return -1;
+//     // Minimum possible answer is the largest book.
+//     int low = *max_element(arr.begin(), arr.end());
 
-    // Maximum possible answer is the sum of all pages.
-    int high = accumulate(arr.begin(), arr.end(), 0);
-    for(int i=low; i<=high; i++){
-        int cntPainter = countPainter(arr, i);
-        if(cntPainter == painter) return i;
-    }
-    return -1;
-}
+//     // Maximum possible answer is the sum of all pages.
+//     int high = accumulate(arr.begin(), arr.end(), 0);
+//     for(int i=low; i<=high; i++){
+//         int cntPainter = countPainter(arr, i);
+//         if(cntPainter == painter) return i;
+//     }
+//     return -1;
+// }
 
 
 // binary search
-// int findPages(vector<int> &arr, int n, int student){
-//    int low = *max_element(arr.begin(), arr.end());
-//    int high = accumulate(arr.begin(), arr.end(), 0);
-//    while(low <= high){
-//     int mid = low +(high - low)/2;
-//     int cntStu = countStudents(arr, mid);
-//     if(cntStu > student) low = mid + 1;
-//     else high = mid - 1;
-//    }
-//    return low;
-// }
+int findtime(vector<int> &arr, int n, int painter){
+   int low = *max_element(arr.begin(), arr.end());
+   int high = accumulate(arr.begin(), arr.end(), 0);
+   while(low <= high){
+    int mid = low +(high - low)/2;
+    int cntStu = countPainter(arr, mid);
+    if(cntStu > painter) low = mid + 1;
+    else high = mid - 1;
+   }
+   return low;
+}
 
 int main(){
     int n;
