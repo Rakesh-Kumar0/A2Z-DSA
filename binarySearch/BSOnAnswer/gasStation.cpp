14d6929e99c8945d16d8aa8272166f3da5a79936
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-07-24 12:25:34 +0530

    gas station

--- binarySearch/BSOnAnswer/gasStation.cpp
+++ binarySearch/BSOnAnswer/gasStation.cpp
@@ -37,24 +37,56 @@ using namespace std;
 
 
 // better approch
+// long double minimizeMaxDistanc(vector<int> &arr, int k){
+//     int n = arr.size();
+//     vector<int> howMany(n-1 , 0);
+//     priority_queue<pair<long double, int>> pq;
+//     for(int i=0; i<n-1; i++){
+//         pq.push({arr[i+1] - arr[i], i});
+//     }
+//     for(int gasStations=1; gasStations<=k; gasStations++){
+//         auto tp = pq.top();
+//         pq.pop();
+//         int sectionInd = tp.second;
+//         howMany[sectionInd]++;
+//         long double iniDiff = arr[sectionInd + 1] - arr[sectionInd];
+//         long double  newSecLen = iniDiff / (long double)(howMany[sectionInd] +1);
+//         pq.push({newSecLen, sectionInd});
+//     }
+    
+//     return pq.top().first;
+//}
+
+// binary search
+int noOfGasStationsRequired(vector<int> &arr, long double dist){
+    int cnt = 0;
+    for(int i=1; i<arr.size(); i++){
+        int numberInBetween = ((arr[i] - arr[i-1])/dist);
+        if((arr[i] - arr[i-1])/dist == numberInBetween*dist){
+            numberInBetween--;
+        }
+        cnt +=numberInBetween;
+    }
+    return cnt;
+}
+
 long double minimizeMaxDistanc(vector<int> &arr, int k){
     int n = arr.size();
-    vector<int> howMany(n-1 , 0);
-    priority_queue<pair<long double, int>> pq;
+    long double low = 0;
+    long double high = 0;
     for(int i=0; i<n-1; i++){
-        pq.push({arr[i+1] - arr[i], i});
+        high = max(high, (long double)(arr[i+1] - arr[i]));
     }
-    for(int gasStations=1; gasStations<=k; gasStations++){
-        auto tp = pq.top();
-        pq.pop();
-        int sectionInd = tp.second;
-        howMany[sectionInd]++;
-        long double iniDiff = arr[sectionInd + 1] - arr[sectionInd];
-        long double  newSecLen = iniDiff / (long double)(howMany[sectionInd] +1);
-        pq.push({newSecLen, sectionInd});
+    long double diff = 1e-6;
+    while(high - low > diff){
+        long double mid = (low + high)/(2.0);
+        int count = noOfGasStationsRequired(arr, mid);
+        if(count>k){
+            low = mid;
+        }
+        else high = mid;
     }
-    
-    return pq.top().first;
+    return high;
 }
 int main(){
     int n;
