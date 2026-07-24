7ca558717e965463b2b7b61ea1ab328b0a92ac60
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-07-24 11:31:49 +0530

    gas station

---
+++ binarySearch/BSOnAnswer/gasStation.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+#include<algorithm>
+#include<numeric>
+#include<cmath>
+using namespace std;
+
+// brute force approch
+long double minimizeMaxDistanc(vector<int> &arr, int k){
+    int n = arr.size();
+    vector<int> howMany(n-1 , 0);
+    for(int gasStations=1; gasStations<=k; gasStations++){
+        long double maxSection = -1;
+        int maxInd = -1;
+        for(int i=0; i<n-1; i++){
+            long double diff = (arr[i+1] - arr[i]);
+            long double sectionLength = diff / (long double)(howMany[i] + 1);
+            if(sectionLength > maxSection){
+                maxSection = sectionLength;
+                maxInd = i;
+
+            }
+        }
+        howMany[maxInd]++;
+    }
+    long double maxAns = -1;
+    for(int i=0; i<n-1; i++){
+        long double diff = (arr[i+1] - arr[i]);
+        long double sectionLength = diff / (long double)(howMany[i] + 1);
+        maxAns = max(maxAns, sectionLength);
+    }
+    return maxAns;
+}
+
+int main(){
+    int n;
+    cout<<"Enter size of an array : ";
+    cin>>n;
+    vector<int> arr;
+    cout<<"Enter elements of an array : ";
+    for(int i=0; i<n; i++){
+
+        int temp;
+        cin>>temp;
+        arr.push_back(temp);
+    }
+    int k ;
+    cout<<"Enter the numbers of new gasstation to be placed : ";
+    cin>>k;
+    int result =minimizeMaxDistanc(arr, k);
+    cout<<result;
+    return 0;
+}
