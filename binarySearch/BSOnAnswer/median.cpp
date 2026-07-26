25eac19a55271457a5e809f4ac2269c42ef1bc01
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-07-26 19:24:14 +0530

    median of two sorted array

--- binarySearch/BSOnAnswer/median.cpp
+++ binarySearch/BSOnAnswer/median.cpp
@@ -42,49 +42,79 @@ using namespace std;
 
 
 // better approch
+// double median(vector<int> &arr1, vector<int> &arr2){
+//     int n1 = arr1.size();
+//     int n2 = arr2.size();
+//     int n = n1 + n2;
+//     int i=0; 
+//     int j=0;
+//     int ind2 = n/2;
+//     int ind1 = ind2 -1;
+//     int cnt = 0;
+//     int ind1ele = -1;
+//     int ind2ele = -1;
+//     while(i < n1 && j < n2){
+//         if(arr1[i] < arr2[j]){
+//             if(cnt == ind1) ind1ele = arr1[i];
+//             if(cnt == ind2) ind2ele = arr1[i];
+//             cnt++;
+//             i++;
+//         }
+//         else{
+//             if(cnt == ind1) ind1ele = arr2[j];
+//             if(cnt == ind2) ind2ele = arr2[j];
+//             cnt++;
+//             j++;
+//         }
+//     }
+//     while(i < n1){
+//         if(cnt == ind1) ind1ele = arr1[i];
+//         if(cnt == ind2) ind2ele = arr1[i];
+//         cnt++;
+//         i++;
+//     }
+//     while(j < n2){
+//         if(cnt == ind1) ind1ele = arr2[j];
+//         if(cnt == ind2) ind2ele = arr2[j];
+//         cnt++;
+//         j++;
+//     }
+    
+//     if(n % 2 == 1){
+//         return ind2ele;
+//     }
+    
+//     return (double)((double)ind1ele + (double)ind2ele)/2.0;
+    
+// }
+
+// optimal approch
 double median(vector<int> &arr1, vector<int> &arr2){
     int n1 = arr1.size();
     int n2 = arr2.size();
     int n = n1 + n2;
-    int i=0; 
-    int j=0;
-    int ind2 = n/2;
-    int ind1 = ind2 -1;
-    int cnt = 0;
-    int ind1ele = -1;
-    int ind2ele = -1;
-    while(i < n1 && j < n2){
-        if(arr1[i] < arr2[j]){
-            if(cnt == ind1) ind1ele = arr1[i];
-            if(cnt == ind2) ind2ele = arr1[i];
-            cnt++;
-            i++;
+    if(n1 > n2) return median(arr2, arr1);
+    int low = 0; 
+    int high = n1;
+    int left = (n1 + n2 +1)/2;
+    while(low <= high){
+        int mid1 = (low + high) >> 1 ; // right shift by 1 means divided by 2;
+        int mid2 = left -mid1;
+        int l1 = INT_MIN, l2 = INT_MIN;
+        int r1 = INT_MAX, r2 = INT_MAX;
+        if(mid1 < n1) r1 = arr1[mid1];
+        if(mid2 < n2) r2 = arr2[mid2];
+        if(mid1 -1 >= 0) l1 = arr1[mid1 - 1];
+        if(mid2 -1 >=0 ) l2 = arr2[mid2 - 1];
+        if(l1 <= r2 && l2 <= r1){
+            if(n % 2 == 1) return max(l1, l2);
+            return (double)(max(l1, l2) + min(r1, r2))/2.0;
         }
-        else{
-            if(cnt == ind1) ind1ele = arr2[j];
-            if(cnt == ind2) ind2ele = arr2[j];
-            cnt++;
-            j++;
-        }
-    }
-    while(i < n1){
-        if(cnt == ind1) ind1ele = arr1[i];
-        if(cnt == ind2) ind2ele = arr1[i];
-        cnt++;
-        i++;
-    }
-    while(j < n2){
-        if(cnt == ind1) ind1ele = arr2[j];
-        if(cnt == ind2) ind2ele = arr2[j];
-        cnt++;
-        j++;
+        else if(l1 > r2) high = mid1 -1;
+        else low = mid1 + 1;
     }
     
-    if(n % 2 == 1){
-        return ind2ele;
-    }
-    
-    return (double)((double)ind1ele + (double)ind2ele)/2.0;
+    return 0;
     
 }
 
