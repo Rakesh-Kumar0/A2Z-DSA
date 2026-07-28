76737d369f4ada8431a2cd72348792f87b7830d1
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-07-28 21:27:52 +0530

    search element

--- binarySearch/2D/searchInMatrix.cpp
+++ binarySearch/2D/searchInMatrix.cpp
@@ -15,16 +15,23 @@ using namespace std;
 
 
 // better approch
-// function that search element in each row
-// bool searchInRow(vector<int> &mat, int m, int target){
-//     for(int i=0; i<m; i++){
-//         if(mat[i] == target) return true;
+// bool binarySearch(vector<int>& nums, int target) {
+//         int n = nums.size();
+//         int low = 0, high = n - 1;
+//         while (low <= high) {
+//             int mid = (low + high) / 2;
+//             if (nums[mid] == target)
+//                 return true;
+//             else if (target > nums[mid])
+//                 low = mid + 1;
+//             else
+//                 high = mid - 1;
+//         }
+//         return false;
 //     }
-//     return false;
-// }
 // bool searchElement(vector<vector<int>> &mat, int n, int m, int target){
 //     for(int i=0; i<n; i++){
-//         if(mat[i][0] <= target && target <= mat[i][m-1]) return searchInRow(mat[i], m, target);
+//         if(mat[i][0] <= target && target <= mat[i][m-1]) return binarySearch(mat[i], target);
 //     }
 //     return false;
 // }
