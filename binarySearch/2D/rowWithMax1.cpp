8d2042e94dbd8616f972d40b21bdbf367c4ee783
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-07-28 13:22:46 +0530

    row with max no. 1's

--- binarySearch/2D/rowWithMax1.cpp
+++ binarySearch/2D/rowWithMax1.cpp
@@ -4,14 +4,44 @@
 using namespace std;
 
 // brute force
+// int rowWithMax1s(vector<vector<int>> &mat, int n, int m){
+//     int maxCnt = -1;
+//     int index = -1;
+//     for(int i=0; i<n; i++){
+//         int cntOnes = 0;
+//         for(int j=0; j<m; j++){
+//             cntOnes +=mat[i][j];
+//         }
+//         if(cntOnes > maxCnt){
+//             maxCnt = cntOnes;
+//             index = i;
+//         }
+//     }
+//     return index;
+// }
+
+// binary search
+
+// function that count no. of 1 in each row
+int lowerBound(vector<int> &arr, int n, int x){
+    int low = 0;
+    int high = n-1;
+    int ans = n;
+    while(low <= high){
+        int mid = low + (high - low)/2;
+        if(arr[mid] >= x){
+            ans = mid;
+            high = mid -1;
+        }
+        else low = mid + 1;
+    }
+    return ans;
+}
 int rowWithMax1s(vector<vector<int>> &mat, int n, int m){
     int maxCnt = -1;
     int index = -1;
     for(int i=0; i<n; i++){
-        int cntOnes = 0;
-        for(int j=0; j<m; j++){
-            cntOnes +=mat[i][j];
-        }
+        int cntOnes = m - lowerBound(mat[i], m, 1);
         if(cntOnes > maxCnt){
             maxCnt = cntOnes;
             index = i;
