9b05cde26a5205ca66b3b5e7ba69b9a4c7dcb7c3
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-07-28 12:04:04 +0530

    kth element in two sorted array

--- binarySearch/BSOnAnswer/kthElement.cpp
+++ binarySearch/BSOnAnswer/kthElement.cpp
@@ -42,80 +42,68 @@ using namespace std;
 
 
 // better approch
-// double median(vector<int> &arr1, vector<int> &arr2){
-//     int n1 = arr1.size();
-//     int n2 = arr2.size();
-//     int n = n1 + n2;
-//     int i=0; 
-//     int j=0;
-//     int ind2 = n/2;
-//     int ind1 = ind2 -1;
-//     int cnt = 0;
-//     int ind1ele = -1;
-//     int ind2ele = -1;
-//     while(i < n1 && j < n2){
-//         if(arr1[i] < arr2[j]){
-//             if(cnt == ind1) ind1ele = arr1[i];
-//             if(cnt == ind2) ind2ele = arr1[i];
-//             cnt++;
-//             i++;
-//         }
-//         else{
-//             if(cnt == ind1) ind1ele = arr2[j];
-//             if(cnt == ind2) ind2ele = arr2[j];
-//             cnt++;
-//             j++;
-//         }
-//     }
-//     while(i < n1){
-//         if(cnt == ind1) ind1ele = arr1[i];
-//         if(cnt == ind2) ind2ele = arr1[i];
-//         cnt++;
-//         i++;
-//     }
-//     while(j < n2){
-//         if(cnt == ind1) ind1ele = arr2[j];
-//         if(cnt == ind2) ind2ele = arr2[j];
-//         cnt++;
-//         j++;
-//     }
-    
-//     if(n % 2 == 1){
-//         return ind2ele;
-//     }
-    
-//     return (double)((double)ind1ele + (double)ind2ele)/2.0;
-    
-// }
-
-// optimal approch
 double kthElement(vector<int> &arr1, vector<int> &arr2, int k){
     int n1 = arr1.size();
     int n2 = arr2.size();
     int n = n1 + n2;
-    if(n1 > n2) return kthElement(arr2, arr1, k);
-    int low = max(0, k-n2); 
-    int high = min(k, n1);
-    int left = k;
-    while(low <= high){
-        int mid1 = (low + high) >> 1 ; // right shift by 1 means divided by 2;
-        int mid2 = left -mid1;
-        int l1 = INT_MIN, l2 = INT_MIN;
-        int r1 = INT_MAX, r2 = INT_MAX;
-        if(mid1 < n1) r1 = arr1[mid1];
-        if(mid2 < n2) r2 = arr2[mid2];
-        if(mid1 -1 >= 0) l1 = arr1[mid1 - 1];
-        if(mid2 -1 >=0 ) l2 = arr2[mid2 - 1];
-        if(l1 <= r2 && l2 <= r1){
-            return max(l1, l2);
+    int i=0; 
+    int j=0;
+    int indx = k - 1; // because of 0 base indexing that is why -1
+    int cnt = 0;
+    while(i < n1 && j < n2){
+        if(arr1[i] < arr2[j]){
+            if(cnt == indx) return arr1[i];
+            cnt++;
+            i++;
         }
-        else if(l1 > r2) high = mid1 -1;
-        else low = mid1 + 1;
+        else{
+            if(cnt == indx) return arr2[j];
+            cnt++;
+            j++;
+        }
     }
-    
+    while(i < n1){
+        if(cnt == indx) return arr1[i];
+        cnt++;
+        i++;
+    }
+    while(j < n2){
+        if(cnt == indx) return arr2[j];
+        cnt++;
+        j++;
+    }
     return 0;
     
 }
+
+// optimal approch
+// double kthElement(vector<int> &arr1, vector<int> &arr2, int k){
+//     int n1 = arr1.size();
+//     int n2 = arr2.size();
+//     int n = n1 + n2;
+//     if(n1 > n2) return kthElement(arr2, arr1, k);
+//     int low = max(0, k-n2); 
+//     int high = min(k, n1);
+//     int left = k;
+//     while(low <= high){
+//         int mid1 = (low + high) >> 1 ; // right shift by 1 means divided by 2;
+//         int mid2 = left -mid1;
+//         int l1 = INT_MIN, l2 = INT_MIN;
+//         int r1 = INT_MAX, r2 = INT_MAX;
+//         if(mid1 < n1) r1 = arr1[mid1];
+//         if(mid2 < n2) r2 = arr2[mid2];
+//         if(mid1 -1 >= 0) l1 = arr1[mid1 - 1];
+//         if(mid2 -1 >=0 ) l2 = arr2[mid2 - 1];
+//         if(l1 <= r2 && l2 <= r1){
+//             return max(l1, l2);
+//         }
+//         else if(l1 > r2) high = mid1 -1;
+//         else low = mid1 + 1;
+//     }
+    
+//     return 0;
+    
+// }
 
 int main(){
     int n1, n2;
