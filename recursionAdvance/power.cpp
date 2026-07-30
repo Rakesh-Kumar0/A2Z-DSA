4e005c5d3c2a5245b89b173ddd799860b52c59c3
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-07-30 10:52:23 +0530

    power function

--- recursionAdvance/power.cpp
+++ recursionAdvance/power.cpp
@@ -5,19 +5,34 @@
 using namespace std;
 
 // brute force
-double power(double x, int n){
+// double power(double x, int n){
+//     // base case
+//     if(n == 0 || x == 1.0) return 1;
+//     long long temp = n; // to avoid integer overflow
+//     if(n < 0){
+//         x = 1/x;
+//         temp = -1 *1LL* n; // make n positive
+//     }
+//     double ans = 1;
+//     for(long long i=1; i<=temp; i++){
+//         ans = ans * x;
+//     }
+//     return ans;
+// }
+
+// using recurssion
+double power(double x, long long n){
     // base case
-    if(n == 0 || x == 1.0) return 1;
-    long long temp = n; // to avoid integer overflow
-    if(n < 0){
-        x = 1/x;
-        temp = -1 *1LL* n; // make n positive
+    if(n == 0) return 1.0;
+    if(n == 1) return x;
+    if(n % 2 == 0){
+        return power(x*x, n/2);
     }
-    double ans = 1;
-    for(long long i=1; i<=temp; i++){
-        ans = ans * x;
-    }
-    return ans;
+    return x* power(x, n-1);
+}
+double myPower(double x, int n){
+    if(n < 0) return (1.0 /power(x, -1*n));
+    return power(x , n);
 }
 int main(){
     double x;
@@ -26,7 +41,7 @@ int main(){
     int n;
     cout<<"Enter power number : ";
     cin>>n;
-    double result = power(x, n);
+    double result = myPower(x, n);
     cout<<result;
     return 0;
 }
