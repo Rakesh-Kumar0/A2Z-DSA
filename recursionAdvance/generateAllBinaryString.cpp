a01e4c04725e1a2989965986b75788b19dd02bc5
Rakesh-Kumar0 <rakeshk49312@gmil.com>
2026-08-03 11:40:26 +0530

    generate all binary string

---
+++ recursionAdvance/generateAllBinaryString.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<stack>
+#include<algorithm>
+using namespace std;
+
+void generate(int n, string curr, vector<string> &result){
+    // base case
+    if(curr.length() == n){
+        result.push_back(curr);
+        return ;
+    }
+    // Always try adding '0'
+    generate(n, curr + "0", result);
+
+    // Add '1' only if previous char is not '1'
+    if(curr.empty() || curr.back() != '1'){
+        generate(n, curr + "1", result);
+    }
+}
+
+int main(){
+    int n;
+    cout<<"Enter length of the string : ";
+    cin>>n;
+    vector<string> result; // store result
+    generate(n, "", result);
+    for(string &s : result){
+        cout<<s <<" ";
+    }
+    cout<<endl;
+
+    return 0;
+
+}
