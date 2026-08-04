5e7d34651ccc081aa02559379ce7a5d7bdb28077
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-08-04 10:58:38 +0530

    generate paranthesis

--- recursionAdvance/generateParanthesis.cpp
+++ recursionAdvance/generateParanthesis.cpp
@@ -7,17 +7,38 @@
 #include<algorithm>
 using namespace std;
 
-void generate(int n, string curr, vector<string> &result){
-    
+// Check if a string has valid parentheses
+bool isValid(string s){
+    int balance = 0;
+    for(char c : s){
+        if(c == '(') balance++;
+        else balance--;
+        if(balance < 0) return false;
+    }
+    if(balance == 0) return true;
+    return false;
+}
+void generateAll(int n, string curr, vector<string> &res){
+    if(curr.length() == 2 * n){
+        if(isValid(curr)) res.push_back(curr);
+        return ;
+    }
+    generateAll(n, curr + '(', res);
+    generateAll(n, curr + ')', res);
+}
+
+vector<string> generateParanthesis(int n){
+    vector<string> res;
+    generateAll(n, "", res);
+    return res;
 }
 
 int main(){
     int n;
     cout<<"Enter length of the string : ";
     cin>>n;
-    vector<string> result; // store result
-    generate(n, "", result);
-    for(string &s : result){
+    vector<string> result = generateParanthesis(n);
+    for(string s : result){
         cout<<s <<" ";
     }
     cout<<endl;
