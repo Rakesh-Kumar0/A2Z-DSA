788226fe0cf0920c6c05bd80da32335992b0105a
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-08-04 11:43:36 +0530

    generate paranthesis

--- recursionAdvance/generateParanthesis.cpp
+++ recursionAdvance/generateParanthesis.cpp
@@ -7,29 +7,47 @@
 #include<algorithm>
 using namespace std;
 
+// brute force
 // Check if a string has valid parentheses
-bool isValid(string s){
-    int balance = 0;
-    for(char c : s){
-        if(c == '(') balance++;
-        else balance--;
-        if(balance < 0) return false;
-    }
-    if(balance == 0) return true;
-    return false;
-}
-void generateAll(int n, string curr, vector<string> &res){
+// bool isValid(string s){
+//     int balance = 0;
+//     for(char c : s){
+//         if(c == '(') balance++;
+//         else balance--;
+//         if(balance < 0) return false;
+//     }
+//     if(balance == 0) return true;
+//     return false;
+// }
+// void generateAll(int n, string curr, vector<string> &res){
+//     if(curr.length() == 2 * n){
+//         if(isValid(curr)) res.push_back(curr);
+//         return ;
+//     }
+//     generateAll(n, curr + '(', res);
+//     generateAll(n, curr + ')', res);
+// }
+
+// vector<string> generateParanthesis(int n){
+//     vector<string> res;
+//     generateAll(n, "", res);
+//     return res;
+// }
+
+
+// optimal approch
+void backtraking(string curr, int open, int close, int n, vector<string> &res){
     if(curr.length() == 2 * n){
-        if(isValid(curr)) res.push_back(curr);
+        res.push_back(curr);
         return ;
     }
-    generateAll(n, curr + '(', res);
-    generateAll(n, curr + ')', res);
+    if(open < n) backtraking(curr + '(', open + 1, close, n, res);
+    if(close < open) backtraking(curr + ')', open, close + 1, n, res);
 }
 
 vector<string> generateParanthesis(int n){
     vector<string> res;
-    generateAll(n, "", res);
+    backtraking("", 0, 0, n, res);
     return res;
 }
 
