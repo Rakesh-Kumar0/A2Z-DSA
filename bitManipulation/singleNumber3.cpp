d687978c7c5c920cf0bf6b18b70f6bb29ab127a0
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-08-18 12:37:47 +0530

    single number 3

--- bitManipulation/singleNumber3.cpp
+++ bitManipulation/singleNumber3.cpp
@@ -4,16 +4,33 @@
 #include<algorithm>
 using namespace std;
 // brute force
-vector<int> singleElement(vector<int> &arr){
-    map<int, int> mpp;
-    for(int i=0; i<arr.size(); i++){
-        mpp[arr[i]]++;
-    }
-    vector<int> ans;
-    for(auto it : mpp){
-        if(it.second == 1) ans.push_back(it.first);
-    }
-    return ans;
+// vector<int> singleElement(vector<int> &arr){
+//     map<int, int> mpp;
+//     for(int i=0; i<arr.size(); i++){
+//         mpp[arr[i]]++;
+//     }
+//     vector<int> ans;
+//     for(auto it : mpp){
+//         if(it.second == 1) ans.push_back(it.first);
+//     }
+//     return ans;
+// }
+
+
+// optimal using bucket concept
+pair<int, int> singleElement(vector<int> &arr){
+   long long xorr = 0;
+   for(int i=0; i<arr.size(); i++){
+    xorr = xorr ^ arr[i];
+   }
+   int rightmost = xorr &(xorr-1)^xorr;
+   int bucket1 = 0;
+   int bucket2 = 0;
+   for(int i=0; i<arr.size(); i++){
+        if(arr[i] & rightmost) bucket1 = bucket1 ^ arr[i];
+        else bucket2 = bucket2 ^ arr[i];
+   }
+   return {bucket1, bucket2};
 }
 int main(){
     int n;
@@ -26,9 +43,7 @@ int main(){
         cin>>temp;
         arr.push_back(temp);
     }
-    vector<int> result = singleElement(arr);
-    for(auto it : result){
-        cout<<it <<" ";
-    }
+    pair<int, int> result = singleElement(arr);
+    cout<<result.first <<" "<<result.second;
     return 0;
 }
