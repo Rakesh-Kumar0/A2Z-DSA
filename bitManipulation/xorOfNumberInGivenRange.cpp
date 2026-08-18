78d9ff9513d2ecbf01e89b61ac1a0b7b360dcd72
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-08-18 11:02:11 +0530

    find xor in the range

--- bitManipulation/xorOfNumberInGivenRange.cpp
+++ bitManipulation/xorOfNumberInGivenRange.cpp
@@ -3,13 +3,22 @@
 #include<algorithm>
 using namespace std;
 
-// optimal solution
+//brute force
 int xorInRange(int n){
-    if(n%4 == 1) return 1;
-    else if(n%4 == 2) return n+1;
-    else if(n%4 == 3) return 0;
-    else return n;
+    int ans = 0;
+    for(int i=1; i<=n; i++){
+        ans = ans^i;
+    }
+    return ans;
 }
+
+// optimal solution
+// int xorInRange(int n){
+//     if(n%4 == 1) return 1;
+//     else if(n%4 == 2) return n+1;
+//     else if(n%4 == 3) return 0;
+//     else return n;
+// }
 int main(){
     int n;
     cout<<"Enter value of n : ";
