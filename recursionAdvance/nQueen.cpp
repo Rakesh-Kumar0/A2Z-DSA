7a62f4083c0554e56ce837267e25ef3d6d7533f0
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-08-22 15:00:54 +0530

    N queen problem

---
+++ recursionAdvance/nQueen.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<climits>
+#include<algorithm>
+using namespace std;
+
+bool isSafe1(int row, int col, vector<string> board, int n){
+    // check upper diagonal
+    int duprow = row;
+    int dupcol = col;
+    while(row >=0 && col >=0){
+        if(board[row][col] == 'Q') return false;
+        row--;
+        col--;
+    }
+
+    row = duprow;
+    col = dupcol;
+    while(col >= 0){
+        if(board[row][col] == 'Q') return false;
+        col--;
+    }
+    row = duprow;
+    col = dupcol;
+    while(row<n && col>=0){
+        if(board[row][col] == 'Q') return false;
+        row++;
+        col--;
+    }
+    return true;
+}
+void solve(int col, vector<string> &board, vector<vector<string>> &ans, int n){
+    if(col == n){
+        ans.push_back(board);
+        return;
+    }
+    for(int row=0; row<n; row++){
+        if(isSafe1(row, col, board, n)){
+            board[row][col] = 'Q';
+            solve(col+1, board, ans, n);
+            board[row][col] = '.';
+        }
+    }
+}
+
+vector<vector<string>> solveNQueens(int n){
+    vector<vector<string>> ans;
+    vector<string> board(n);
+    string s(n, '.');
+    for(int i=0; i<n; i++){
+        board[i] =s;
+    }
+    solve(0, board, ans, n);
+    return ans;
+    
+}
+
+int main(){
+    int n;
+    cout<<"Enter no. of queen : ";
+    cin>>n;
+    vector<vector<string>> result = solveNQueens(n);
+    for(auto row : result){
+        for(auto it : row){
+            cout<<it <<" "<<endl;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
