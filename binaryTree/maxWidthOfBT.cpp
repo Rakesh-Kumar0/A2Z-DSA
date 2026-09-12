3a1d51e27eace05400c6efca6d54e89c4eb1957c
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-09-12 10:25:00 +0530

    maximum width of binary tree

---
+++ binaryTree/maxWidthOfBT.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<stack>
+#include<climits>
+#include<queue>
+#include<map>
+#include<set>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+
+    // constructor
+    Node(int value){
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+int widthOfBinaryTree(Node* root){
+    if(root == NULL) return 0;
+    queue<pair<Node* , int>> q;
+    int width = 0;
+    q.push({root, 0});
+    while(!q.empty()){
+        int size = q.size();
+        int min = q.front().second;
+        int first, last;
+        for(int i=0; i<size; i++){
+            int cur_id = q.front().second-min;
+            Node* node = q.front().first;
+            q.pop();
+            if(i == 0) first = cur_id;
+            if(i == size-1) last = cur_id;
+            if(node->left) q.push({node->left, i*2+1});
+            if(node->right) q.push({node->right, 2*1+2});
+
+        }
+        width = max(width, last-first+1);
+    }
+    return width;
+}
+
+int main(){
+    // creating the root node
+    Node* root = new Node(1);
+    root->left = new Node(3);
+    root->right = new Node(7);
+    root->left->left = new Node(8);
+    root->right->right = new Node(4);
+    Node* p = root->left;
+    Node* q = root->right;
+    int result = widthOfBinaryTree(root);
+    cout<<result;
+    return 0;
+}
