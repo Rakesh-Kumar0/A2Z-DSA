58c69bd8fa2ecff9e70f7d521cc8043b79639c16
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-09-14 21:26:52 +0530

    children sum property

---
+++ binaryTree/childrenSumProperty.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<stack>
+#include<climits>
+#include<queue>
+#include<map>
+#include<set>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+
+    // constructor
+    Node(int value){
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+void changeTree(Node* root){
+    if(root == NULL) return;
+    int child = 0;
+    if(root->left){
+        child += root->left->data;
+    }
+    if(root->right){
+        child += root->right->data;
+    }
+    if(child >= root->data) root->data = child;
+    else{
+        if(root->left) root->left->data = root->data;
+        if(root->right) root->right->data = root->data;
+    }
+
+    changeTree(root->left);
+    changeTree(root->right);
+    int tot = 0;
+    if(root->left) tot += root->left->data;
+    if(root->right) tot += root->right->data;
+    if(root->left || root->right) root->data = tot;
+}
+
+// Function to print the inorder
+// traversal of the tree
+void inorderTraversal(Node* root) {
+    if (root == nullptr) {
+        return;
+    }
+    inorderTraversal(root->left);
+    cout << root->data << " ";
+    inorderTraversal(root->right);
+}
+
+int main(){
+    // creating the root node
+    Node* root = new Node(40);
+    root->left = new Node(10);
+    root->right = new Node(20);
+    root->left->left = new Node(2);
+    root->left->right = new Node(5);
+    root->right->left = new Node(30);
+    root->right->right = new Node(40);
+    changeTree(root);
+    inorderTraversal(root);
+    return 0;
+}
